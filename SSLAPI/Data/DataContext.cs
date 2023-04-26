504ae571a37008c2c03cba26dc4fe921f6af34dc
junxian428 <holeyiho@gmail.com>
2023-04-26 22:39:14 +0800

    update

--- SSLAPI/Data/DataContext.cs
+++ SSLAPI/Data/DataContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SSLAPI.Models;
 
 namespace SSLAPI.Data
 {
@@ -7,6 +8,31 @@ namespace SSLAPI.Data
 
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Country> Countries { get; set; }
+
+        public DbSet<Owner> Owners { get; set; }
+
+        public DbSet<Pokemon> Pokemon { get; set; }
+
+        public DbSet<PokemonOwner> PokemonOwners { get; set; }
+
+        public DbSet<PokemonCategory> PokemonCategories { get; set; }
+
+        public DbSet<Review> Reviews { get; set; }
+
+        public DbSet<Reviewer> Reviewers { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PokemonCategory>()
+                .HasKey(pc => new {pc.PokemonId, pc.CategoryId});
+          
 
         }
     }
