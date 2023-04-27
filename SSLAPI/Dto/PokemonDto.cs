bcf0c242a77d0bc73f02fe2c2c2cd2ad5f1bd756
junxian428 <holeyiho@gmail.com>
2023-04-27 16:26:27 +0800

    update

---
+++ SSLAPI/Dto/PokemonDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SSLAPI.Dto
+{
+    public class PokemonDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public DateTime BirthDate { get; set; }
+    }
+}
